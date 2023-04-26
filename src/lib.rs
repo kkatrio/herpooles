9f2fe5aaf029df7211acc5f1ebbccd86aaddd819
konstantinos katrioplas <kokatrio@gmail.com>
2023-04-26 21:16:00 +0300

    move herpooles up down left right

--- src/lib.rs
+++ src/lib.rs
@@ -5,21 +5,23 @@ use utils::set_panic_hook;
 
 #[wasm_bindgen]
 pub struct Herpooles {
-    xh: f64,
-    yh: f64,
+    pub x: f64,
+    pub y: f64,
 }
 
 #[wasm_bindgen]
 impl Herpooles {
     #[wasm_bindgen(constructor)]
-    pub fn new(xh: f64, yh: f64) -> Herpooles {
-        Herpooles { xh, yh }
+    pub fn new(x: f64, y: f64) -> Herpooles {
+        Herpooles { x, y }
     }
+    // use set method, if we keep the data in js
 }
 #[wasm_bindgen]
 pub fn draw(ctx: &web_sys::CanvasRenderingContext2d, h: &Herpooles) {
     set_panic_hook();
-    create_herpooles(ctx, &h);
+    ctx.clear_rect(0.0, 0.0, 1000.0, 800.0);
+    draw_herpooles(ctx, &h);
 }
 
 #[wasm_bindgen(start)]
@@ -27,7 +29,6 @@ fn main() -> Result<(), JsValue> {
     Ok(())
 }
 
-fn create_herpooles(ctx: &web_sys::CanvasRenderingContext2d, h: &Herpooles) {
-    ctx.rect(h.xh, h.yh, 20.0, 20.0);
-    ctx.stroke();
+fn draw_herpooles(ctx: &web_sys::CanvasRenderingContext2d, h: &Herpooles) {
+    ctx.fill_rect(h.x, h.y, 20.0, 20.0);
 }
