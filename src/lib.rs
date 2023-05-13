84f88270070c62b1e78367f70dbf95b75a927614
konstantinos katrioplas <kokatrio@gmail.com>
2023-05-13 12:37:42 +0300

    play controls, game over, distance

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,7 @@ use utils::set_panic_hook;
 pub struct Herpooles {
     pub x: f64,
     pub y: f64,
+    pub alive: bool,
 }
 
 #[wasm_bindgen]
@@ -17,35 +18,61 @@ pub struct Zombie {
 
 #[wasm_bindgen]
 impl Herpooles {
-    #[wasm_bindgen(constructor)]
-    pub fn new(x: f64, y: f64) -> Herpooles {
-        Herpooles { x, y }
+    #[wasm_bindgen(constructor)] // TODO: why is this needed
+    pub fn new() -> Herpooles {
+        Herpooles {
+            x: 500.0,
+            y: 500.0,
+            alive: true,
+        }
     }
 }
 #[wasm_bindgen]
 impl Zombie {
     #[wasm_bindgen(constructor)]
-    pub fn new(x: f64, y: f64) -> Zombie {
-        Zombie { x, y }
+    pub fn new() -> Zombie {
+        Zombie { x: 500.0, y: 400.0 }
     }
 }
+
 #[wasm_bindgen]
-pub fn draw(ctx: &web_sys::CanvasRenderingContext2d, h: &Herpooles, z: &Zombie) {
+pub fn draw(ctx: &web_sys::CanvasRenderingContext2d, h: &mut Herpooles, z: &Zombie) {
     set_panic_hook();
     ctx.clear_rect(1.0, 1.0, 998.0, 798.0);
-    draw_herpooles(ctx, &h);
-    draw_zombie(ctx, z);
+    // is herpooles alive? => determine color
+
+    // TODO: use an enum
+    let herpooles_state = if is_herpooles_alive(h, z) {
+        "black"
+    } else {
+        "red"
+    };
+
+    draw_herpooles(ctx, &h, herpooles_state);
+    draw_zombie(ctx, z, "orange");
+
+    if herpooles_state == "red" {
+        log!("herpooles state red!");
+        h.alive = false
+    }
 }
 
-fn draw_herpooles(ctx: &web_sys::CanvasRenderingContext2d, h: &Herpooles) {
-    ctx.set_fill_style(&JsValue::from_str("black"));
+fn draw_herpooles(ctx: &web_sys::CanvasRenderingContext2d, h: &Herpooles, c: &str) {
+    ctx.set_fill_style(&JsValue::from_str(c));
     ctx.fill_rect(h.x, h.y, 20.0, 20.0);
 }
 
-fn draw_zombie(ctx: &web_sys::CanvasRenderingContext2d, h: &Zombie) {
-    ctx.set_fill_style(&JsValue::from_str("red"));
+fn draw_zombie(ctx: &web_sys::CanvasRenderingContext2d, h: &Zombie, c: &str) {
+    ctx.set_fill_style(&JsValue::from_str(c));
     ctx.fill_rect(h.x, h.y, 20.0, 20.0);
 }
+
+fn is_herpooles_alive(h: &Herpooles, z: &Zombie) -> bool {
+    let d = (h.x - z.x) * (h.x - z.x) + (h.y - z.y) * (h.y - z.y);
+    log!("d: {}", d);
+    d > 400.0 // TODO: calculate based on herpooles and zombie area
+}
+
 #[wasm_bindgen(start)]
 fn main() -> Result<(), JsValue> {
     Ok(())
--- src/utils.rs
+++ src/utils.rs
@@ -1,16 +1,9 @@
+#[allow(unused_macros)]
 macro_rules! log {
-    // Note that this is using the `log` function imported above during
-    // `bare_bones`
     ($($t:tt)*) => (web_sys::console::log_1(&format!($($t)*).into()))
 }
 
 pub fn set_panic_hook() {
-    // When the `console_error_panic_hook` feature is enabled, we can call the
-    // `set_panic_hook` function at least once during initialization, and then
-    // we will get better error messages if our code ever panics.
-    //
-    // For more details see
-    // https://github.com/rustwasm/console_error_panic_hook#readme
     #[cfg(feature = "console_error_panic_hook")]
     console_error_panic_hook::set_once();
 }
